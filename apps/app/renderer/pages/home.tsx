b63ee5befd6a3f1d959ca50431b9c02cd04a9a70
Jason Pang <jason@jasonhpang.com>
2023-08-21 17:46:17 -0700

    Initial commit

---
+++ apps/app/renderer/pages/home.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Head from "next/head";
+import Link from "next/link";
+import { Button } from "@mui/joy";
+
+function Home() {
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Home - Nextron (with-typescript)</title>
+      </Head>
+      <div>
+        <p>
+          ⚡ Electron + Next.js ⚡ -<Button variant="solid">Hello world</Button>
+          <Link href="/next">
+            <a>Go to next page</a>
+          </Link>
+        </p>
+        <img src="/images/logo.png" />
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default Home;
---
+++ apps/pianocheat-old/renderer/next-env.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="next" />
+/// <reference types="next/types/global" />
+declare module 'webaudiofont'
+declare module 'simple-deep-freeze'
